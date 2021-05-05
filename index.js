2299c8141e960afc69be37b7e4a9b5ad7b79972f
Iryna Stein <isophiestein@gmail.com>
2021-05-05 17:53:21 -0600

    commit tree 8194f4eb401878eb3f73e5664773815f170332e8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+
+// const newText = document.getElementById('text')
+// newText.innerHTML = "This is really cool"
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const newText = document.getElementById('text')
+//     newText.innerHTML = "This is really cool!";
+//   });
+
+document.addEventListener("DOMContentLoaded", nameChange)
+
+function nameChange(){
+document.getElementById('text').innerHTML = "This is really cool!"; 
+};
